cc24a26b6121078488c130e8a159f528ce21bf86
Gois <matheusgoislimasilva@gmail.com>
2021-10-23 00:56:12 -0300

    set example with swiftui

--- LanScannerExample/Shared/ContentView.swift
+++ LanScannerExample/Shared/ContentView.swift
@@ -8,9 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @Environment(\.scenePhase) private var scenePhase
+    @ObservedObject var viewModel = CountViewModel()
+    @State var showAlert: Bool = false
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading) {
+            VStack {
+                HStack {
+                    Text(viewModel.title)
+                        .font(.title)
+                    Spacer()
+                    Button {
+                        viewModel.reload()
+                    } label: {
+                        Image(systemName: "repeat")
+                            .foregroundColor(.black)
+                    }
+                }
+                ProgressView(value: viewModel.progress)
+            }.padding()
+
+            List {
+                ForEach(viewModel.connectedDevices) { device in
+                    VStack(alignment: .leading) {
+                        Text(device.name)
+                            .font(.body)
+                        Text(device.mac)
+                            .font(.caption)
+                        Text(device.brand)
+                            .font(.footnote)
+                    }
+                    .onTapGesture {
+                        UIPasteboard.general.string = device.name
+                    }
+                    .padding()
+
+                }
+            }.alert(isPresented: $viewModel.showAlert) {
+                Alert(
+                    title: Text("Scan Finished"),
+                    message: Text("Number of devices connected to your Local Area Network: \(viewModel.connectedDevices.count)")
+                )
+            }
+        }
     }
 }
 
---
+++ LanScannerExample/Shared/ContentViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  CountViewModel.swift
+//  LanScannerExample
+//
+//  Created by Matheus Gois on 23/10/21.
+//
+
+import SwiftUI
+import LanScanner
+
+class CountViewModel: ObservableObject {
+
+    // Properties
+
+    @Published var connectedDevices = [LanDevice]()
+    @Published var progress: CGFloat = .zero
+    @Published var title: String = .init()
+    @Published var showAlert = false
+
+    private lazy var scanner = LanScanner(delegate: self)
+
+    // Init
+
+    init() {
+        scanner.start()
+    }
+
+    // Methos
+
+    func reload() {
+        connectedDevices.removeAll()
+        scanner.start()
+    }
+}
+
+extension CountViewModel: LanScannerDelegate {
+    func lanScanHasUpdatedProgress(_ progress: CGFloat, address: String) {
+        self.progress = progress
+        self.title = address
+    }
+
+    func lanScanDidFindNewDevice(_ device: LanDevice) {
+        connectedDevices.append(device)
+    }
+
+    func lanScanDidFinishScanning() {
+        showAlert = true
+    }
+}
+
+extension LanDevice: Identifiable {
+    public var id: UUID { .init() }
+}
